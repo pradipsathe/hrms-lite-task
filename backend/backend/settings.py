cc9bc66b14041d0167c9c6ed97a18c63472f486d
pradipsathe <pradipsathe33@gmail.com>
2026-02-28 16:04:32 +0530

    Change setting for production

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ['*']
 
