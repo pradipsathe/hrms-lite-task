f1dd490f86b5757f69ba27e7bc7e558dccb60793
pradipsathe <pradipsathe33@gmail.com>
2026-03-02 11:21:52 +0530

    update attendance functionality

--- backend/hrms/views.py
+++ backend/hrms/views.py
@@ -1,22 +1,38 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from .models import Employee, Attendance
 from .serializers import EmployeeSerializers, AttendanceSerializers
-from rest_framework.decorators import action
 from rest_framework.response import Response
 
-# Create your views here.
 
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializers
-    
+
+
 class AttendanceViewSet(viewsets.ModelViewSet):
     queryset = Attendance.objects.all()
     serializer_class = AttendanceSerializers
-    
+
     def get_queryset(self):
         employee_id = self.request.query_params.get('employee_id')
         if employee_id:
             return self.queryset.filter(employee_id=employee_id)
         return self.queryset
+
+    def create(self, request, *args, **kwargs):
+        employee = request.data.get('employee')
+        date = request.data.get('date')
+
+        attendance = Attendance.objects.filter(
+            employee_id=employee,
+            date=date
+        ).first()
+
+        if attendance:
+            serializer = self.get_serializer(attendance, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return super().create(request, *args, **kwargs)
